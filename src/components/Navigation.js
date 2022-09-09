8cfd0827d8530ebf1ddeaba81a4b4c27ccb4ad74
Hanna <90796659+HannaTylna@users.noreply.github.com>
2022-09-09 21:27:26 +0200

    add fetch api menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import $ from "jquery";
 
-export default function Navigation() {
- $(document).ready(function () {
-  $(".header__burger").click(function (event) {
-   $(".header__burger, .header__menu").toggleClass("active");
-   $("body").toggleClass("lock");
-  });
+$(document).ready(function () {
+ $(".header__burger").click(function (event) {
+  $(".header__burger, .header__menu").toggleClass("active");
+  $("body").toggleClass("lock");
  });
-
+});
+export default function Navigation() {
+ const [menu, setMenu] = useState("");
+ useEffect(() => {
+  const url =
+   "http://localhost:8888/wordpress/wp-json/wp/v2/menu-items?menus=3";
+  const token = localStorage.getItem("exam");
+  fetch(url, {
+   method: "GET",
+   headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${token}`,
+   },
+  })
+   .then((res) => res.json())
+   .then((data) => {
+    console.log(data);
+    setMenu(data);
+   });
+ }, []);
  return (
   <div className="wrapper">
    <header className="header">
@@ -22,19 +39,19 @@ export default function Navigation() {
       </div>
       <nav className="header__menu">
        <ul className="header__list">
+        {menu &&
+         menu.map((item, index) => {
+          return (
+           <li key={index} className="header__item">
+            <a href={item.url} className="header__link">
+             {item.title.rendered}
+            </a>
+           </li>
+          );
+         })}
         <li className="header__item">
-         <a href="/" className="header__link">
-          Home
-         </a>
-        </li>
-        <li className="header__item">
-         <a href="/about" className="header__link">
-          About me
-         </a>
-        </li>
-        <li className="header__item">
-         <a href="/contact" className="header__link">
-          Contact
+         <a href="/login" className="header__link">
+          Logout
          </a>
         </li>
        </ul>
