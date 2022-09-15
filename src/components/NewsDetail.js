23aa813cc83103be67b3ccb00d8aaff42793b659
Hanna <90796659+HannaTylna@users.noreply.github.com>
2022-09-15 23:01:49 +0200

    fetch comments

--- src/components/NewsDetail.js
+++ src/components/NewsDetail.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from "react";
 import parse from "html-react-parser";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+ display: flex;
+`;
 
 export default function NewsDetail(props) {
  const [newsDetail, setNewsDetail] = useState("");
+ const [comments, setComments] = useState([]);
  const navigate = useNavigate();
 
  useEffect(() => {
@@ -24,10 +30,40 @@ export default function NewsDetail(props) {
   }
   fetchData();
  }, [props.id]);
+
+ useEffect(() => {
+  function fetchData() {
+   const url = `${process.env.REACT_APP_API_URL}wp/v2/comments?posts${props.id}`;
+   const token = localStorage.getItem("exam");
+   fetch(url, {
+    method: "GET",
+    headers: {
+     "Content-Type": "application/json",
+     Authorization: `Bearer ${token}`,
+    },
+   })
+    .then((res) => res.json())
+    .then((data) => {
+     console.log(data);
+     setComments(data);
+    });
+  }
+  fetchData();
+ }, [props.id]);
  return (
   <div className="news__content">
    <button onClick={() => navigate(-1)}>Gå tillbaka</button>
    {newsDetail.content && <div>{parse(newsDetail.content.rendered)}</div>}
+   <h3>Comments</h3>
+   {comments &&
+    comments.map((comment) => {
+     return (
+      <StyledDiv key={comment.id}>
+       <h4>{comment.author_name}</h4>
+       <p>{parse(comment.content.rendered)}</p>
+      </StyledDiv>
+     );
+    })}
   </div>
  );
 }
