7b2d7153ab7cde7658d9206c45a47ba6ee8dca4a
Hanna <90796659+HannaTylna@users.noreply.github.com>
2022-09-15 21:46:50 +0200

    add sort by category

--- src/pages/NewsListPage.js
+++ src/pages/NewsListPage.js
@@ -1,13 +1,20 @@
 import React, { useState, useEffect } from "react";
-// import parse from "html-react-parser";
+import parser from "html-react-parser";
 import Navigation from "../components/Navigation";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+ padding: 5px;
+ margin-left: 15px;
+`;
 
 export default function NewsListPage(props) {
- const [result, setResult] = useState([]);
- useEffect(() => {
-  const url = `${process.env.REACT_APP_API_URL}wp/v2/posts`;
-  const token = localStorage.getItem("exam");
+ const [categories, setCategories] = useState([]);
+ const [postsByCategory, setPostsByCategory] = useState([]);
 
+ const getCategories = () => {
+  const url = `${process.env.REACT_APP_API_URL}wp/v2/categories`;
+  const token = localStorage.getItem("exam");
   fetch(url, {
    method: "GET",
    headers: {
@@ -17,16 +24,63 @@ export default function NewsListPage(props) {
   })
    .then((res) => res.json())
    .then((data) => {
-    setResult(data);
+    setCategories(data);
+   });
+ };
+
+ const getPostByCategory = (category) => {
+  const url = `${process.env.REACT_APP_API_URL}wp/v2/posts?categories=${category.id}`;
+  fetch(url, {
+   method: "GET",
+  })
+   .then((res) => res.json())
+   .then((data) => {
+    setPostsByCategory(data);
+   });
+ };
+
+ useEffect(() => {
+  const url = `${process.env.REACT_APP_API_URL}wp/v2/posts`;
+  const token = localStorage.getItem("exam");
+
+  fetch(url, {
+   method: "GET",
+
+   headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${token}`,
+   },
+  })
+   .then((res) => res.json())
+   .then((data) => {
+    setPostsByCategory(data);
    });
  }, []);
+
+ useEffect(() => {
+  getCategories();
+ }, []);
+
  return (
   <>
    <Navigation />
    <div className="content">
     <h1 className="content__header">Nyheter</h1>
+    <div>
+     <h2>Categories</h2>
+     {categories &&
+      categories.map((category) => {
+       return (
+        <StyledButton
+         key={category.id}
+         onClick={() => getPostByCategory(category)}>
+         {parser(category.name)}
+        </StyledButton>
+       );
+      })}
+    </div>
     <div className="cards">
-     {result.map((item, index) => {
+     {postsByCategory.map((item, index) => {
       return (
        <div key={index} className="card">
         <h5 className="card-title">{item.title.rendered}</h5>
