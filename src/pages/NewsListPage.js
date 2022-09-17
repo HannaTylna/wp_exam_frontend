65da932ca82633b7278df1a46d25205d60a0c5cc
Hanna <90796659+HannaTylna@users.noreply.github.com>
2022-09-17 21:59:57 +0200

    delete filter by tags

--- src/pages/NewsListPage.js
+++ src/pages/NewsListPage.js
@@ -10,50 +10,11 @@ const StyledButton = styled.button`
 
 export default function NewsListPage(props) {
  const [categories, setCategories] = useState([]);
- const [tags, setTags] = useState([]);
  const [postsByCategory, setPostsByCategory] = useState([]);
  const [pagesByCategory, setPagesByCategory] = useState([]);
- const [postsByTag, setPostsByTag] = useState([]);
- const [pagesByTag, setPagesByTag] = useState([]);
 
- //  function getCategories() {
- //   const url = `${process.env.REACT_APP_API_URL}wp/v2/categories`;
- //   fetch(url, {
- //    method: "GET",
- //    headers: {
- //     "Content-Type": "application/json",
- //    },
- //   })
- //    .then((res) => res.json())
- //    .then((data) => {
- //     setCategories(data);
- //    });
- //  }
-
- //  function getPostByCategory(category) {
- //   const url = `${process.env.REACT_APP_API_URL}wp/v2/posts?categories=${category.id}`;
- //   fetch(url, {
- //    method: "GET",
- //   })
- //    .then((res) => res.json())
- //    .then((data) => {
- //     setPostsByCategory(data);
- //    });
- //  }
-
- //  function getPagesByCategory(category) {
- //   const url = `${process.env.REACT_APP_API_URL}wp/v2/pages?categories=${category.id}`;
- //   fetch(url, {
- //    method: "GET",
- //   })
- //    .then((res) => res.json())
- //    .then((data) => {
- //     setPagesByCategory(data);
- //    });
- //  }
-
- function getTags() {
-  const url = `${process.env.REACT_APP_API_URL}wp/v2/tags`;
+ function getCategories() {
+  const url = `${process.env.REACT_APP_API_URL}wp/v2/categories`;
   fetch(url, {
    method: "GET",
    headers: {
@@ -62,28 +23,29 @@ export default function NewsListPage(props) {
   })
    .then((res) => res.json())
    .then((data) => {
-    setTags(data);
+    setCategories(data);
    });
  }
- function getPagesByTag(tag) {
-  const url = `${process.env.REACT_APP_API_URL}wp/v2/pages?tags=${tag.id}`;
+
+ function getPostByCategory(category) {
+  const url = `${process.env.REACT_APP_API_URL}wp/v2/posts?categories=${category.id}`;
   fetch(url, {
    method: "GET",
   })
    .then((res) => res.json())
    .then((data) => {
-    setPagesByTag(data);
+    setPostsByCategory(data);
    });
  }
- function getPostByTag(tag) {
-  const url = `${process.env.REACT_APP_API_URL}wp/v2/posts?tags=${tag.id}`;
+
+ function getPagesByCategory(category) {
+  const url = `${process.env.REACT_APP_API_URL}wp/v2/pages?categories=${category.id}`;
   fetch(url, {
    method: "GET",
   })
    .then((res) => res.json())
    .then((data) => {
-    console.log(data);
-    setPostsByTag(data);
+    setPagesByCategory(data);
    });
  }
 
@@ -124,8 +86,28 @@ export default function NewsListPage(props) {
  }, []);
 
  useEffect(() => {
-  // getCategories();
-  getTags();
+  getCategories();
+  // getTags();
+ }, []);
+
+ useEffect(() => {
+  const url = `${process.env.REACT_APP_API_URL}wp/v2/blocks
+
+  `;
+  const token = localStorage.getItem("exam");
+
+  fetch(url, {
+   method: "GET",
+
+   headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${token}`,
+   },
+  })
+   .then((res) => res.json())
+   .then((data) => {
+    console.log(data);
+   });
  }, []);
 
  return (
@@ -133,7 +115,7 @@ export default function NewsListPage(props) {
    <Navigation />
    <div className="content">
     <h1 className="content__header">Home</h1>
-    {/* <div>
+    <div>
      <h2>Categories</h2>
      {categories &&
       categories.map((category) => {
@@ -164,46 +146,6 @@ export default function NewsListPage(props) {
     </div>
     <div className="cards">
      {pagesByCategory.map((item, index) => {
-      return (
-       <div key={index} className="card">
-        <h5 className="card-title">{item.title.rendered}</h5>
-        <a href={`/filmer/${item.id}`} className="card-link">
-         Mer...
-        </a>
-       </div>
-      );
-     })}
-    </div> */}
-    <div>
-     <h2>Tags:</h2>
-     {tags &&
-      tags.map((tag) => {
-       return (
-        <StyledButton
-         key={tag.id}
-         onClick={() => {
-          getPagesByTag(tag);
-          getPostByTag(tag);
-         }}>
-         {parser(tag.name)}
-        </StyledButton>
-       );
-      })}
-    </div>
-    <div className="cards">
-     {postsByTag.map((item, index) => {
-      return (
-       <div key={index} className="card">
-        <h5 className="card-title">{item.title.rendered}</h5>
-        <a href={`/${item.id}`} className="card-link">
-         Mer...
-        </a>
-       </div>
-      );
-     })}
-    </div>
-    <div className="cards">
-     {pagesByTag.map((item, index) => {
       return (
        <div key={index} className="card">
         <h5 className="card-title">{item.title.rendered}</h5>
